020b2a9077ea30a752d47dee99585a5b8b53c6f2
Adewumi <owonikokoadewunmi@gmail.com>
2022-11-01 01:49:50 +0100

    Fixed anchor link

--- src/Profile/PSpace.jsx
+++ src/Profile/PSpace.jsx
@@ -1,9 +1,12 @@
 import "./PSpace.css"
-import slack from "../images/vector.svg"
+// import slack from "../images/vector.svg"
 import github from "../images/Icon.svg"
 import vectom from "../images/vectom.png"
 import footertext from "../images/Footer text.png"
 import I4G from "../images/I4G.png"
+import slack from "../images/slack.png"
+import Adewumi from "../images/Adewumi.jpg"
+
 import { useState } from "react";
 
 import { FaEllipsisH, FaShare , FaCamera } from "react-icons/fa";
@@ -12,54 +15,61 @@ export default function PSpace() {
   const linksArray = [
 
     {
-      link:"https//twitter.com/Bee_dPrrogrammer",
-      content: "Twitter Link"
+      link:"https://twitter.com/Bee_dProgrammer",
+      content: "Twitter Link",
+      title:"This is the link to my twiter page"
     },
     {
-      link:"http://training.zuri.team/",
+      link:"https://training.zuri.team/",
       content: "Zuri Team",
-      id :" btn_zuri"
+      id :" btn__zuri",
+      title:"The Zuri Team"
     },
     {
-      link:"https//books.zuri.team",
+      link:"https://books.zuri.team",
       content: "Zuri Books",
-      id:"books"
+      id:"books",
+      title:"Learn more from Zuri library"
     },
     {
-      link:"https//books.zuri.team/python-for-beginners?ref_id=<Adewumi>",
+      link:"https://books.zuri.team/python-for-beginners?ref_id=<Adewumi>",
       content: "Python Books",
-      id:"python_book"
+      id:"python_book",
+      title:"Beginner to expert Python guide"
     },
     {
-      link:"https//background.zuri.team",
+      link:"https://background.zuri.team",
       content: "Background Check for Colors",
-      id:"pitch"
+      id:"pitch",
+      title:"The Zuri color theme"
     },
 
     {
-     link:"https//books.zuri.team",
+     link:"https://books.zuri.team",
      content: "Design Books",
-     id:"book_design"
+     id:"book_design",
+     title:"Zuri Design books"
   }
   ]
-  const [toggles,setToggles] = useState(false);
-   
-  const handleClick =() => {
- setToggles( !toggles );
-  }
+  
   const [profile,setProfile] = useState(false);
   
   return ( 
     <div className="pSpace"> 
     <div className="header">
 
-       <div className="toggle" onClick={handleClick}>
-     {toggles? <FaEllipsisH/> : <FaShare/> }
+       <div className="toggle">
+        <div className="desktopIcon">
+       <FaShare/> 
+       </div>
+       <div className="mobileIcon">
+       <FaEllipsisH/> 
+       </div>
       </div>
       
        <div className="person">
        <figure onClick={()=> setProfile(!profile)}>
-        <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Pretty lady"  id="profile_img"/>
+        <img src={Adewumi} alt="Pretty lady"  id="profile__img"/>
       {profile && <div className="camera"> <FaCamera/></div>}
      <figcaption>
           <p id="twitter">Adewumi</p>
@@ -73,9 +83,8 @@ export default function PSpace() {
 
        <div className="links" >
         {linksArray.map((links,id)=>{
-          return  <div className="training" key={id}>
-          <button id="my_twitter"> <a href={links.link}>{links.content}</a></button>
-        </div>
+          return  <a href={links.link} target="_blank" rel="noreferrer" title={links.title} id={links.id} className="training" key={id}>{links.content}</a>
+        
         })}
         </div>
 
@@ -83,7 +92,7 @@ export default function PSpace() {
         
 
       <div className="icons" >
-        <a href="https://app.slack.com" target="_blank"  rel="noreferrer"> <img src={slack} alt="" /> </a>
+        <div className="slack"><a href="https://app.slack.com" target="_blank"  rel="noreferrer" > <img src={slack} alt="" /> </a></div>
         <a href="https://github.com/Jejeet" target="_blank"  rel="noreferrer"><img src={github} alt="" /></a>
 
         </div>
