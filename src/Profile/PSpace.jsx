35b0fdaece85408e6142d28f63f1c5c5bad26009
Adewumi <owonikokoadewunmi@gmail.com>
2022-11-05 17:39:21 +0100

    contact page media responsiveness

--- src/Profile/PSpace.jsx
+++ src/Profile/PSpace.jsx
@@ -1,115 +1,138 @@
-import "./PSpace.css"
+import "./PSpace.css";
 // import slack from "../images/vector.svg"
-import github from "../images/Icon.svg"
-import vectom from "../images/Zuri.png"
-import footertext from "../images/Footer text.png"
-import I4G from "../images/I4G.png"
-import slack from "../images/slack.png"
-import Adewumi from "../images/Adewumi.jpg"
-import share from "../images/share.png"
-import escp from "../images/esclipsis.png"
-import cam from "../images/cam.png"
+import github from "../images/Icon.svg";
+import vectom from "../images/Zuri.png";
+import footertext from "../images/Footer text.png";
+import I4G from "../images/I4G.png";
+import slack from "../images/slack.png";
+import Adewumi from "../images/Adewumi.jpg";
+import share from "../images/share.png";
+import escp from "../images/esclipsis.png";
+import cam from "../images/cam.png";
 
 import { useState } from "react";
-import { Link } from "react-router-dom"
-
-
+import { Link } from "react-router-dom";
 
 export default function PSpace() {
   const linksArray = [
-
     {
-      link:"https://twitter.com/Bee_dProgrammer",
+      link: "https://twitter.com/Bee_dProgrammer",
       content: "Twitter Link",
-      title:"This is the link to my twiter page"
+      title: "This is the link to my twiter page",
     },
     {
-      link:"https://training.zuri.team/",
+      link: "https://training.zuri.team/",
       content: "Zuri Team",
-      id :" btn__zuri",
-      title:"The Zuri Team"
+      id: " btn__zuri",
+      title: "The Zuri Team",
     },
     {
-      link:"https://books.zuri.team",
+      link: "https://books.zuri.team",
       content: "Zuri Books",
-      id:"books",
-      title:"Learn more about design and coding from Zuri library."
+      id: "books",
+      title: "Learn more about design and coding from Zuri library.",
     },
     {
-      link:"https://books.zuri.team/python-for-beginners?ref_id=<Adewumi>",
+      link: "https://books.zuri.team/python-for-beginners?ref_id=<Adewumi>",
       content: "Python Books",
-      id:"python__book",
-      title:"Beginner to expert Python guide . It is affordable."
+      id: "python__book",
+      title: "Beginner to expert Python guide . It is affordable.",
     },
     {
-      link:"https://background.zuri.team",
+      link: "https://background.zuri.team",
       content: "Background Check for Colors",
-      id:"pitch",
-      title:"The Zuri color theme is available and affordable"
+      id: "pitch",
+      title: "The Zuri color theme is available and affordable",
     },
 
     {
-     link:"https://books.zuri.team",
-     content: "Design Books",
-     id:"book__design",
-     title:"This is a free Design books from Zuri. At Zuri, we care about good design and we are happy to help with your design"
-  },
-  ]
-  
-  const [profile,setProfile] = useState(false);
-  
-  return ( 
-    <div className="pSpace"> 
-    <div className="header">
+      link: "https://books.zuri.team",
+      content: "Design Books",
+      id: "book__design",
+      title:
+        "This is a free Design books from Zuri. At Zuri, we care about good design and we are happy to help with your design",
+    },
+  ];
 
-       <div className="toggle">
-        <div className="desktopIcon">
-       <img src={share} alt="" /> 
-       </div>
-       <div className="mobileIcon">
-       <img src={escp} alt="" /> 
-       </div>
+  const [profile, setProfile] = useState(false);
+
+  return (
+    <div className="pSpace">
+      <div className="header">
+        <div className="toggle">
+          <div className="desktopIcon">
+            <img src={share} alt="" />
+          </div>
+          <div className="mobileIcon">
+            <img src={escp} alt="" />
+          </div>
+        </div>
+
+        <div className="person">
+          <figure onClick={() => setProfile(!profile)}>
+            <img src={Adewumi} alt="Pretty lady" id="profile__img" />
+            {profile && (
+              <div className="camera">
+                {" "}
+                <img src={cam} alt="" />
+              </div>
+            )}
+            <figcaption>
+              <p id="twitter">Adewumi</p>
+              <p id="slack">Adewumi</p>
+            </figcaption>
+          </figure>
+        </div>
       </div>
-      
-       <div className="person">
-       <figure onClick={()=> setProfile(!profile)}>
-        <img src={Adewumi} alt="Pretty lady"  id="profile__img"/>
-      {profile && <div className="camera"> <img src={cam} alt="" /></div>}
-     <figcaption>
-          <p id="twitter">Adewumi</p>
-          <p id="slack" >Adewumi</p>
-        </figcaption>
-       </figure>
 
-       </div>
-      
-       </div>
-
-       <div className="links" >
-        {linksArray.map((links,id)=>{
-          return  <a href={links.link} target="_blank" rel="noreferrer" title={links.title} id={links.id} className="training" key={id}>{links.content}</a>
-        
+      <div className="links">
+        {linksArray.map((links, id) => {
+          return (
+            <a
+              href={links.link}
+              target="_blank"
+              rel="noreferrer"
+              title={links.title}
+              id={links.id}
+              className="training"
+              key={id}
+            >
+              {links.content}
+            </a>
+          );
         })}
-        <Link to = "/contact" className="training" id="contact">Contact</Link>
-        </div>
+        <Link to="/contact" className="training" id="contact">
+          Contact
+        </Link>
+      </div>
 
-       <div className="nav">
-      <div className="icons" >
-        <div className="slack"><a href="https://app.slack.com" target="_blank"  rel="noreferrer" > <img src={slack} alt="" /> </a></div>
-        <a href="https://github.com/Jejeet" target="_blank"  rel="noreferrer"><img src={github} alt="" /></a>
-
+      <div className="nav">
+        <div className="icons">
+          <div className="slack">
+            <a href="https://app.slack.com" target="_blank" rel="noreferrer">
+              {" "}
+              <img src={slack} alt="" />{" "}
+            </a>
+          </div>
+          <a href="https://github.com/Jejeet" target="_blank" rel="noreferrer">
+            <img src={github} alt="" />
+          </a>
         </div>
-       </div>
-       <hr />
-       <div className="footer">
+      </div>
+      <hr />
+      <div className="footer">
         <footer>
-          <div><img src={vectom} alt="" /></div>
-          <p><img src={footertext} alt="" /></p>
-          <div><img src={I4G} alt="" /></div>
+          <div>
+            <img src={vectom} alt="" />
+          </div>
+          <p>
+            <img src={footertext} alt="" />
+          </p>
+          <div>
+            <img src={I4G} alt="" />
+          </div>
         </footer>
-
-       </div>
-
+      </div>
     </div>
-  )
+  );
 }
--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -1,117 +1,141 @@
-import React from 'react'
-import "./contact.css"
-import vectom from "../images/Zuri.png"
-import footertext from "../images/Footer text.png"
-import I4G from "../images/I4G.png"
-import { useState,useRef } from "react"
+import React from "react";
+import "./contact.css";
+import vectom from "../images/Zuri.png";
+import footertext from "../images/Footer text.png";
+import I4G from "../images/I4G.png";
+import { useState, useRef } from "react";
 
-
-const Contact =()=>{
-  const [message,setMessage] = useState("");
-  const [firstName,setFirstName] = useState("");
-  const [email,setEmail] = useState("");
-  const [lastName,setLastName] = useState("");
-  const [lastNameError,setLastNameError] = useState(false);
-  const [firstNameError,setFirstNameError] = useState(false);
-  const[emailError, setEmailError] = useState("");
+const Contact = () => {
+  const [message, setMessage] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [lastNameError, setLastNameError] = useState(false);
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [emailError, setEmailError] = useState("");
   const [error, setError] = useState(false);
 
-  const textRef = useRef()
+  const textRef = useRef();
 
-  
-  const handleSubmit =(e) => {
- e.preventDefault();
- if(message === "" ){
-  setError(true)
-  textRef.current.focus()
- } else{
-  setError(false)
- }
- if(firstName === "" ){
-  setFirstNameError(true)
-  
- } else{
-  setFirstNameError(false)
- }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (message === "") {
+      setError(true);
+      textRef.current.focus();
+    } else {
+      setError(false);
+    }
+    if (firstName === "") {
+      setFirstNameError(true);
+    } else {
+      setFirstNameError(false);
+    }
 
- if(lastName === "" ){
-  setLastNameError(true)
+    if (lastName === "") {
+      setLastNameError(true);
+    } else {
+      setLastNameError(false);
+    }
 
- } else{
-  setLastNameError(false)
- }
-
-  if(email === "" ){
-    setEmailError(true)
-   } else{
-    setEmailError(false)
-   }
- 
-  }
+    if (email === "") {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+  };
   return (
-    <div className='contact'>
+    <div className="contact">
+      <form>
+        <h3>Contact Me</h3> <br />
+        <p> Hi there, contact me to ask me about anything you have in mind </p>
+        <div className="body">
+          <div className="persons">
+            <div className="inputs">
+              <label>First name</label>
+              <input
+                type="text"
+                placeholder="Enter first your name"
+                id="first_name"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              {firstNameError ? (
+                <p className="error">Please enter your firstname</p>
+              ) : (
+                ""
+              )}
+            </div>
 
-       <form >
-           
-            <h3>Contact Me</h3> <br />
-            <p> Hi there, contact me to ask me about anything you have in mind </p>
-            
-            <div className="body">
-                <div className='persons'>
+            <div className="inputs">
+              <label>Last name</label>
+              <input
+                type="text"
+                placeholder="Enter your last name"
+                id="last_name"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+              {lastNameError ? (
+                <p className="error">Please enter your lastname</p>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
 
-                <div className="inputs">
-                <label>First name</label>
-                <input type="text"  placeholder='Enter first your name' id='first_name'  onChange={e=>setFirstName(e.target.value)} />
-                {firstNameError? <p className='error'>Please enter your firstname</p> : "" }
-                </div> 
-           
+          <div className="inputs">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="yourname@gmail.com"
+              id="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          {emailError ? (
+            <p className="error">This is not a valid email format</p>
+          ) : ( "" )}
 
-                <div className="inputs">
-                <label>Last name</label>
-                <input type="text"  placeholder='Enter your last name' id='last_name'  onChange={e=>setLastName(e.target.value)}/> 
-                {lastNameError? <p className='error'>Please enter your lastname</p> : "" }
-                </div>
-               
-             </div>
-             
-             <div className= "inputs">
-
-                <label>Email</label> 
-                <input type = "email" placeholder='yourname@gmail.com' id='email' name='email'  onChange={e =>setEmail(e.target.value)} /> 
-              </div> 
-              {emailError? <p className='error'>This is not a valid email format</p> : "" }
-
-                <div className= "inputs">
-                <label>Message</label> 
-                <textarea name="message" id="message" cols="30" rows="5" 
-                placeholder="Send me a message and I'll reply you as soon as possible" 
-                onChange={e=>setMessage(e.target.value)} ref={textRef} ></textarea>
-                </div>
-                {error? <p className='error'>Please enter a message</p> : "" }
-         </div>
-     
-  <div className='check'>
-     <input type="checkbox" id="" name="" value="" className='box' />
-  <label className='checkmessage'> You agree to providing your data to Adewumi who may contact you</label> 
-  </div>
- <button id='btn__submit' type='button' onClick={ e => handleSubmit(e)}  >Send message</button>
-
-</form>
-<hr />
-<div className="footer">
-   
+          <div className="inputs">
+            <label>Message</label>
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              rows="5"
+              placeholder="Send me a message and I'll reply you as soon as possible"
+              onChange={(e) => setMessage(e.target.value)}
+              ref={textRef}
+            ></textarea>
+          </div>
+          {error ? <p className="error">Please enter a message</p> : ""}
+        </div>
+        <div className="check">
+          <input type="checkbox" id="" name="" value="" className="box" />
+          <label className="checkmessage">
+            {" "}
+            You agree to providing your data to Adewumi who may contact you
+          </label>
+        </div>
+        <button id="btn__submit" type="button" onClick={(e) => handleSubmit(e)}>
+          Send message
+        </button>
+      </form>
+      <hr />
+      <div className="footer">
         <footer>
-        
-          <div><img src={vectom} alt="" /></div>
-          <p><img src={footertext} alt="" /></p>
-          <div><img src={I4G} alt="" /></div>
-
+          <div>
+            <img src={vectom} alt="" />
+          </div>
+          <p>
+            <img src={footertext} alt="" />
+          </p>
+          <div>
+            <img src={I4G} alt="" />
+          </div>
         </footer>
-
-  </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
