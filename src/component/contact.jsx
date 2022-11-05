5500f8e7b936d89e3b6f38eb2358479d7d7e85b9
Adewumi <owonikokoadewunmi@gmail.com>
2022-11-05 02:58:12 +0100

    input and textarea styled using UseRef,useState and CSS accordingly

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -3,12 +3,29 @@ import "./contact.css"
 import vectom from "../images/Zuri.png"
 import footertext from "../images/Footer text.png"
 import I4G from "../images/I4G.png"
+import { useState,useRef } from "react"
 
-const contact = () => {
+
+const Contact =()=>{
+  const [message,setMessage] = useState("");
+  const [error, setError] = useState(false);
+
+  const textRef = useRef()
+
+
+  const handleSubmit =(e) => {
+ e.preventDefault();
+ if(message === ""){
+  setError(true)
+  textRef.current.focus()
+ } else{
+  setError(false)
+ }
+  }
   return (
     <div className='contact'>
 
-       <form >
+       <form  >
            
             <h2>Contact Me</h2> <br />
             <p> Hi there, contact me to ask me about anything you have in mind </p>
@@ -18,8 +35,9 @@ const contact = () => {
 
                 <div className="inputs">
                 <label>First name</label>
-                <input type="text"  placeholder='Enter first your name' id='first_name'/>
+                <input type="text"  placeholder='Enter first your name' id='first_name' />
                 </div>
+               
 
                 <div className="inputs">
                 <label>Last name</label>
@@ -35,15 +53,18 @@ const contact = () => {
                 </div> 
                 <div className= "inputs">
                 <label>Message</label> 
-                <textarea name="message" id="message" cols="30" rows="5" placeholder="Send me a message and I'll reply you as soon as possible"  ></textarea>
+                <textarea name="message" id="message" cols="30" rows="5" 
+                placeholder="Send me a message and I'll reply you as soon as possible" 
+                onChange={e=>setMessage(e.target.value)} ref={textRef} ></textarea>
                 </div>
+                {error? <p className='error'>Please enter a message</p> : "" }
      </div>
      
 <div className='check'>
      <input type="checkbox" id="" name="" value="" className='box' />
   <label className='checkmessage'> You agree to providing your data to Adewumi who may contact you</label> <br />
   </div>
-<button id='btn__submit'>Send message</button>
+<button id='btn__submit' type='button' onClick={ e => handleSubmit(e)} >Send message</button>
 
 </form>
 <hr />
@@ -62,4 +83,4 @@ const contact = () => {
   )
 }
 
-export default contact
+export default Contact;
